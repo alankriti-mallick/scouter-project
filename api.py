65da9bf436dd5e4a3039d2628059c3c8b70cb0ef
vishalshaw03 <vsrk243@gmail.com>
2023-05-22 01:16:01 +0530

    api updated

--- api.py
+++ api.py
@@ -6,6 +6,9 @@ from test_custom import run_custom_test
 from typing import Annotated
 import os
 
+CLASS_NAMES = {3: "Pituitary Tumor detected", 0: "No tumor detected",
+               2: "Meningioma Tumor", 2: "Glioma Tumor detected"}
+
 app = FastAPI()
 app.mount("/ui", StaticFiles(directory="ui", html=True), name="ui")
 templates = Jinja2Templates(directory="ui")
@@ -21,14 +24,16 @@ async def root(
     image: Annotated[bytes, File()],
     # name:  Annotated[str, Form()]
 ):
-    with open('test-images/image.jpg', 'wb') as img:
+    with open('test-images/BT/image.jpg', 'wb') as img:
         img.write(image)
         img.close()
 
-    result = run_custom_test(image='image.jpg', device='cpu')
+    result = run_custom_test(
+        image='image.jpg', device='cpu', dataset="BT", batch_size=32)
     # print(result)
+    result = CLASS_NAMES[result]
 
-    if os.path.exists("test-images/image.jpg"):
-        os.remove("test-images/image.jpg")
+    if os.path.exists("test-images/BT/image.jpg"):
+        os.remove("test-images/BT/image.jpg")
 
     return {"data": result}
